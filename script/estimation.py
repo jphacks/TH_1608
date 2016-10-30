64a19030cf7f572684fadb8a0989a571de8035a8
asano <asano@ecei.tohoku.ac.jp>
2016-10-30 12:29:54 +0900

    移動平均

--- script/estimation.py
+++ script/estimation.py
@@ -2,19 +2,34 @@
 
 import sys
 
+import numpy as np
 from scipy import stats
 
 from sent2prob import sentences2probs
 
-# 確率のリストを受け取って別れたかどうかを推定します。実験的なコードです。
-model = '/home/asano/twitter_model3'
-probs = sentences2probs(sys.stdin, model)
-print probs
-recent = stats.hmean(probs[:5])
-past = stats.hmean(probs[6:])
 
-if recent < past:
-    if recent > 0.5 and past - recent > 0.2:
-        print '!'
-print 'recent:{}'.format(recent)
-print 'past:{}'.format(past)
+# 確率のリストを受け取って別れたかどうかを推定します。
+
+
+def main(probs, width, threshold, n_tweets):
+
+    window = np.ones(width) / float(width)
+    sma_lis = np.convolve(probs, window, 'valid')
+
+    broken_up = False
+    for i in range(len(sma_lis) - 1):
+        if sma_lis[i + 1] - sma_lis[i] > threshold:
+            broken_up = True
+
+    if broken_up:
+        print('!')
+    print(stats.hmean(probs[:n_tweets]))
+
+if __name__ == '__main__':
+    model = '/home/asano/twitter_model3'
+    probs = sentences2probs(sys.stdin, model)
+
+    width = 5  # 移動平均の窓幅
+    threshold = 0.1  # 移動平均の降下量の閾値
+    n_tweets = 10  # 恋人がいる確率を出すのに使うツイート数
+    main(probs, width, threshold, n_tweets)
