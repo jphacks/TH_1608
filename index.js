077a546b66d6a8fe46d69d370eb116f1cfa2b9eb
reiyw <reiyw.setuve@gmail.com>
2016-10-30 15:41:12 +0900

    For demo

--- index.js
+++ index.js
@@ -11,32 +11,19 @@ var options = {
     url: 'http://www.cl.ecei.tohoku.ac.jp/~ryo-t/wakarepo/api.py/',
     json: true,
     qs: {
-        'screen_name': 'auzen_,favofavo9'
+        'screen_name': 'favofavo9'
     }
 };
 
+var flag = false;
 
 bot.startRTM(function(err, bot, payload) {
     if (err) {
         throw new Error('Could not connect to Slack');
     }
     new CronJob({
-        cronTime: '* * * * *',
-        onTick: request.get(options, function(error, response, body) {
-            var user = 'favofavo9';
-            if (!error && response.statusCode == 200) {
-                if (body['broken_up'][user]) {
-                    var text = '最近 ' + user + ' さんが別れました！';
-                    bot.say({
-                        channel: 'wakarepo',
-                        text: text,
-                        username: 'わかれぽ'
-                    });
-                }
-            } else {
-                console.log('error: ' + response.statusCode);
-            }
-        }),
+        cronTime: '*/10 * * * * *',
+        onTick: wakarepo,
         start: true,
         timeZone: 'Asia/Tokyo'
     });
@@ -48,3 +35,31 @@ controller.hears(['test'], ['direct_message', 'direct_mention', 'mention'], func
         username: 'わかれぽ'
     });
 });
+
+function wakarepo() {
+    if (!flag) {
+        request.get(options, function(error, response, body) {
+            var user = 'favofavo9';
+            if (!error && response.statusCode == 200) {
+                if (body['broken_up'][user]) {
+                    flag = true;
+                    var text = '最近 ' + user + ' さんが別れました！';
+                    bot.say({
+                        channel: 'wakarepo',
+                        text: text,
+                        username: 'わかれぽ'
+                    });
+                } else {
+                    var text = user + ' さんは *' + String(parseInt(Number(body['probs'][user]) * 100)) + '%* の確率で付き合っています';
+                    bot.say({
+                        channel: 'wakarepo',
+                        text: text,
+                        username: 'わかれぽ'
+                    });
+                }
+            } else {
+                console.log('error: ' + response.statusCode);
+            }
+        })
+    }
+}
